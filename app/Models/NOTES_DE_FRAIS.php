ada099ef83d23512ac3ede1aacdafd86f4d224fb
soulayman <soulaymanjaa09@gmail.com>
2025-06-23 12:17:05 +0100

    creating NotesDeFrais model

--- app/Models/NOTES_DE_FRAIS.php
+++ app/Models/NOTES_DE_FRAIS.php
@@ -1,11 +1,31 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class NoteDeFrais extends Model
 {
     use HasFactory;
+
+    protected $table = 'notes_de_frais';
+
+    protected $fillable = [
+        'utilisateur_id',
+        'date_depense',
+        'categorie',
+        'montant',
+        'devise',
+        'description',
+        'fichier_justificatif',
+        'statut',
+        'commentaire_validation',
+        'date_validation',
+    ];
+
+    public function utilisateur(): BelongsTo
+    {
+        return $this->belongsTo(Utilisateur::class);
+    }
 }
