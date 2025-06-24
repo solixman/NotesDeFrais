e53734ce7049fe2238e880cd16ff5e4218d4d960
soulayman <soulaymanjaa09@gmail.com>
2025-06-24 16:04:59 +0100

    created store method

--- app/Http/Controllers/Api/DeplacementController.php
+++ app/Http/Controllers/Api/DeplacementController.php
@@ -25,4 +25,35 @@ class DeplacementController extends Controller
         }
     }
 
+    
+    public function store(Request $request)
+    {
+        
+        try {
+       
+        $data = $request->validate([
+            'objet' => 'required|string|max:255',
+            'lieu' => 'required|string|max:255',
+            'date_depart' => 'required|date',
+            'date_retour' => 'required|date|after_or_equal:date_depart',
+            'moyen_transport' => 'required|string|max:100',
+            'cout_estime' => 'required|numeric',
+            'description' => 'nullable|string',
+        ]);
+
+        $data['utilisateur_id'] = Auth::id();
+        $data['statut'] = 'en_attente';
+
+        Deplacement::create($data);
+        return ['succes'=>'deplacement created succefully'] ;
+             
+        } catch (Exception $e) {
+            return ['error'=>$e->getMessage()];
+        }
+    }
+
+
+    
+   
+
 }
